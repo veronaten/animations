159ff8432e7ac94670956b2e87fc0bfd3f60cb3c
Veronika <verona.binko@gmail.com>
2022-01-10 15:32:27 +0300

    Feat: add animation eith openModal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,18 +1,40 @@
-import React from 'react';
+// import React from 'react';
 
-import './Modal.css';
+// import './Modal.css';
 
-const modal = (props) => {
+// const modal = (props) => {
 
-  const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
+//   const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
+//   return (
+//     <div className={cssClasses.join(' ')}>
+//       <h1>A Modal</h1>
+//       <button className="Button" onClick={props.closed}>Dismiss</button>
+//     </div>
+//   )
+// };
+
+  
+
+// export default modal;
+
+import React from "react";
+
+import "./Modal.css";
+
+const modal = props => {
+  const cssClasses = [
+    "Modal",
+    props.show ? "ModalOpen" : "ModalClosed"
+  ];
+
   return (
     <div className={cssClasses.join(' ')}>
       <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>Dismiss</button>
+      <button className="Button" onClick={props.closed}>
+        Dismiss
+      </button>
     </div>
-  )
+  );
 };
-
-  
 
 export default modal;
