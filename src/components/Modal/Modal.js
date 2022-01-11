5143002bba2ceb87522f8a777b382662dceacf0d
Veronika <verona.binko@gmail.com>
2022-01-11 09:56:29 +0300

    Refactor: in Modal.js wrap by Transition component with jsx code

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,41 +1,36 @@
-// import React from 'react';
-
-// import './Modal.css';
-
-// const modal = (props) => {
-
-//   const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
-//   return (
-//     <div className={cssClasses.join(' ')}>
-//       <h1>A Modal</h1>
-//       <button className="Button" onClick={props.closed}>Dismiss</button>
-//     </div>
-//   )
-// };
-
-  
-
-// export default modal;
-
 import React from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./Modal.css";
 
 const modal = props => {
-  const cssClasses = [
-    "Modal",
-    props.show === "entering" 
-    ? "ModalOpen"
-    : props.show === "exiting" ? "ModalClosed" : null
-  ];
 
   return (
-    <div className={cssClasses.join(' ')}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <Transition
+      in={props.show} 
+      timeout={300}
+      mountOnEnter
+      unmountOnExit
+    >
+      {state => {
+        const cssClasses = [ 
+          "Modal",
+          state === "entering" 
+          ? "ModalOpen"
+          : state === "exiting"
+          ? "ModalClosaed" : null
+        ];
+        return (
+          <div className={cssClasses.join(' ')}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        )
+      }}
+      
+    </Transition>
   );
 };
 
